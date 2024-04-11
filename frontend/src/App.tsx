7af425354e16076a9402c41af52016df7b9d12f9
sumitnair26 <sumit.nair26@gmail.com>
2024-04-11 23:02:04 +0530

    added websocket logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [message, setMessages] = useState([]);
+  //const [inputText, setInputText] = useState("");
 
   useEffect(() => {
     const newSocket = new WebSocket('ws://localhost:8080');
@@ -12,14 +14,25 @@ function App() {
     }
     newSocket.onmessage = (message) => {
       console.log('Message received:', message.data);
+      setMessages(m => [...m, message.data])
     }
     setSocket(newSocket);
     return () => newSocket.close();
   }, [])
 
+  if(!socket){
   return (
     <>
-      hi there
+      Connecting to socket server .......
+    </>
+  ) }
+  return (
+    <>
+      <input ></input>
+      <button onClick={()=>{
+        socket.send("Sending message from Frontend");
+      }}>Send</button>
+      {message}
     </>
   )
 }
